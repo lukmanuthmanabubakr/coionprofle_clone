ff7cab99ac3c85b90013ea3451adaa13c95eb960
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2023-10-18 19:56:01 +0100

    second commit

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 import HomeSectionOne from './HomeSectionOne/HomeSectionOne'
 import HomeSectionTwo from './HomeSectionTwo/HomeSectionTwo'
+import HomeSectionThree from './HomeSectionThree/HomeSectionThree'
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
         <Navbar />
         <HomeSectionOne />
         <HomeSectionTwo/>
+        <HomeSectionThree/>
     </div>
   )
 }
---
+++ src/Pages/HomePage/HomeSectionThree/HomeSectionThree.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./HomeSectionThree.css"
+import coinImg from "../../../Asset/coin_img.webp"
+
+
+const data = [
+  {
+    id: 1,
+    
+  }
+]
+
+const HomeSectionThree = () => {
+  return (
+    <section className='homeSectionThree'>
+
+    </section>
+  )
+}
+
+export default HomeSectionThree
